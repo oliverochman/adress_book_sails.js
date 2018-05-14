8dad4df745864d8d36fc88f123cfe256973f6790
oliver_ochman <oliver.ochman@hotmail.com>
2018-05-14 11:52:53 +0200

    rough draft of guide finished

---
+++ features/step_definitions/basic-steps.js
@@ -0,0 +1,22 @@
+const  { After, Given, Then, When }  =  require('cucumber')
+
+Given('I visit the site',  async  function()  {
+  return  await  this.openHomePage()
+})
+
+Then('I should see {string}',  async  function(content)  {
+ return await this.sleep(3000);
+ return  await  this.pageHasTextContent(content)
+})
+
+Then('I fill in {string} with {string}', async function(field, content) {
+  return await this.fillFormField(field.toLowerCase(), content);
+})
+
+When('I click {string}',  async  function(string)  {
+ return  await  this.clickOnAddContactBtn()
+})
+
+After(async  function()  {
+  return await this.closeHomePage()
+})
